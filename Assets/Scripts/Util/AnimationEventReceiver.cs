8495d7bc150b85215c10454549c23c8c45e47f07
Kimhyogyeom <gyrua77@gmail.com>
2026-03-17 14:33:20 +0900

    feat: 플레이어 캐릭터 모델 및 애니메이션 연동

---
+++ Assets/Scripts/Util/AnimationEventReceiver.cs
@@ -0,0 +1,14 @@
+/// <summary>
+/// 외부 에셋 애니메이션 이벤트 수신용
+/// - ExplosiveLLC RPG Animation Pack의 FootL/FootR 등 이벤트 처리
+/// - 실제 동작 없이 오류만 없앰
+/// </summary>
+public class AnimationEventReceiver : UnityEngine.MonoBehaviour
+{
+    void FootL() { }
+    void FootR() { }
+    void Hit() { }
+    void Shoot() { }
+    void FootstepL() { }
+    void FootstepR() { }
+}
