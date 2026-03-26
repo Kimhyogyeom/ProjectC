78a7f2f3d41bc054e7b3dcd23cd67a6292756d9e
Kimhyogyeom <gyrua77@gmail.com>
2026-03-26 11:03:28 +0900

    refactor: 표창 투사체 오브젝트 풀링 적용 (Instantiate/Destroy 제거)

--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -1,8 +1,8 @@
-using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 표창 투사체
+/// 표창 투사체 (오브젝트 풀링)
 /// - 직선으로 날아가서 적에게 충돌 시 데미지
 /// - 독, 관통, 데미지 배율 지원
 /// </summary>
@@ -12,6 +12,50 @@ public class Projectile : MonoBehaviour
     [Header("Projectile Settings")]
     [SerializeField] float _lifeTime = 3f;
     [SerializeField] int _damage = 10;
+    #endregion
+
+    #region Pool
+    static readonly Queue<Projectile> _pool = new Queue<Projectile>();
+    static Transform _poolParent;
+
+    static void EnsurePoolParent()
+    {
+        if (_poolParent == null)
+        {
+            _poolParent = new GameObject("[ProjectilePool]").transform;
+            Object.DontDestroyOnLoad(_poolParent.gameObject);
+        }
+    }
+
+    /// <summary>풀에서 투사체를 꺼내거나 새로 생성</summary>
+    public static Projectile Spawn(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+        EnsurePoolParent();
+
+        Projectile proj;
+        if (_pool.Count > 0)
+        {
+            proj = _pool.Dequeue();
+            proj.transform.SetParent(null);
+            proj.transform.position = position;
+            proj.transform.rotation = rotation;
+            proj.gameObject.SetActive(true);
+        }
+        else
+        {
+            GameObject obj = Instantiate(prefab, position, rotation);
+            proj = obj.GetComponent<Projectile>();
+        }
+        return proj;
+    }
+
+    void ReturnToPool()
+    {
+        gameObject.SetActive(false);
+        EnsurePoolParent();
+        transform.SetParent(_poolParent);
+        _pool.Enqueue(this);
+    }
     #endregion
 
     #region Private Fields
@@ -26,12 +70,17 @@ public class Projectile : MonoBehaviour
     bool _isPoisonSpreading = false;    // Lv5 전염
 
     bool _hit = false;
+    float _aliveTimer = 0f;
     #endregion
 
     #region Unity Lifecycle
     void Update()
     {
         transform.Translate(_direction * _speed * Time.deltaTime, Space.World);
+
+        _aliveTimer += Time.deltaTime;
+        if (_aliveTimer >= _lifeTime)
+            ReturnToPool();
     }
     #endregion
 
@@ -56,7 +105,7 @@ public class Projectile : MonoBehaviour
         }
 
         if (!_isPierce)
-            Destroy(gameObject);
+            ReturnToPool();
     }
     #endregion
 
@@ -68,14 +117,15 @@ public class Projectile : MonoBehaviour
         _speed = speed;
         _damageRatio = damageRatio;
         _isPierce = isPierce;
+        _hit = false;
+        _aliveTimer = 0f;
+        _hasPoison = false;
 
         if (!_initialized)
         {
             _damage += UpgradeManager.GetBonusDamage();
             _initialized = true;
         }
-
-        Destroy(gameObject, _lifeTime);
     }
 
     /// <summary>독 설정</summary>
